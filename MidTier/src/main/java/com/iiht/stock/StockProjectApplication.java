eb9f50b9f09b570eb86973300dc19939a7455da6
ZhiHuaGao <zhgao@cn.ibm.com>
2020-05-15 22:11:10 +0800

    1

--- MidTier/src/main/java/com/iiht/stock/StockProjectApplication.java
+++ MidTier/src/main/java/com/iiht/stock/StockProjectApplication.java
@@ -1,9 +1,12 @@
 package com.iiht.stock;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class StockProjectApplication {
 
 	public static void main(String[] args) {
