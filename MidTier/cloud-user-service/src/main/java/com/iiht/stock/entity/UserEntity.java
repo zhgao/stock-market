07eec714ff78c7833398a3e16b8e241de0390057
ZhiHuaGao <zhgao@cn.ibm.com>
2020-05-15 20:49:30 +0800

    update

--- MidTier/cloud-user-service/src/main/java/com/iiht/stock/entity/UserEntity.java
+++ MidTier/cloud-user-service/src/main/java/com/iiht/stock/entity/UserEntity.java
@@ -14,6 +14,7 @@ import org.springframework.data.annotation.Id;
 public class UserEntity {
 	@Id 
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id")
 	private long id;
 	
 	@Column(name="username")
--- MidTier/src/test/java/com/iiht/stock/StockProjectApplicationTests.java
+++ MidTier/src/test/java/com/iiht/stock/StockProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package com.iiht.stock;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StockProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
